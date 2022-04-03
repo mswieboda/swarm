3ee28299c7122ac84d2e5b3860c88033fc6049dd
Matt Swieboda <mswieboda@users.noreply.github.com>
2022-04-02 23:11:44 -0500

    adds wave spawn intervals

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,13 +2,19 @@ extends Spatial
 
 const DISTANCE_MIN = 35
 const DISTANCE_MAX = 45
-const WAVE_ENEMY_MULTIPLIER = 5
-const WAVE_TIMER = 5
+const WAVE_ENEMY_MULTIPLIER = 2
+const WAVE_SPAWNS_PER_INTERVAL = 1
+const WAVE_SPAWN_INTERVAL = 1 * 60
+const WAVE_TIMER = 5 * 60
+const WAVE_WAIT_TIMER = 5
 const GAME_OVER_TIMER = 1
 
 var wave = 0
-var wave_timer = 0
-var game_over_timer = 0
+var wave_time = 0
+var wave_interval_time = 0
+var wave_wait_time = 0
+var game_over_time = 0
+var num_enemies = 0
 var is_next_wave = true
 var is_game_over = false
 
@@ -24,12 +30,15 @@ func _physics_process(delta):
   if Input.is_action_just_pressed("spawn"):
     spawn_enemy(crawler_scene)
 
+  check_num_enemies()
+
   draw_hud()
 
   if is_next_wave:
     next_wave(delta)
   else:
-    check_end_wave()
+    check_wave_spawns(delta)
+    check_end_wave(delta)
     check_end_game(delta)
 
 func spawn_enemy(scene):
@@ -56,59 +65,89 @@ func spawn_vector():
 
   return v
 
-func num_enemies():
-  return $enemies.get_child_count()
+func check_num_enemies():
+  num_enemies = $enemies.get_child_count()
 
 func draw_hud():
-  var enemies = num_enemies()
-
   $hud/margin/vbox/wave.text = "wave: " + str(wave)
-  $hud/margin/vbox/enemies.text = "enemies: " + str(enemies)
+  $hud/margin/vbox/enemies.text = "enemies: " + str(num_enemies)
 
+  var wave_info = ""
   var info = ""
 
   if is_next_wave:
-    info = "next wave starting: " + str(WAVE_TIMER - wave_timer)
-  elif is_game_over:
+    wave_info = "next wave starting: " + show_time(WAVE_WAIT_TIMER - wave_wait_time)
+  else:
+    wave_info = "wave lasts: " + show_time(WAVE_TIMER - wave_time)
+    wave_info += " next enemy in: " + show_time(wave_interval_timer() - wave_interval_time)
+
+  $hud/margin/vbox/wave_info.text = wave_info
+
+  if is_game_over:
     info = "game over"
   else:
     info = "base health: " + str(supply_depot.health)
 
   $hud/margin/vbox/info.text = info
 
+func show_time(time):
+  var mins = floor(time / 60)
+  var secs = time - mins * 60
+
+  var result = ""
+
+  if mins > 0:
+    result += "%d mins " % mins
+
+  return result + "%.1f secs" % secs
+
 func check_end_game(delta):
   if !is_game_over:
     return
 
-  if game_over_timer <= GAME_OVER_TIMER:
-    game_over_timer += delta
+  if game_over_time <= GAME_OVER_TIMER:
+    game_over_time += delta
   else:
     Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
     $hud/margin/center/game_over_dialog.show()
 
-func check_end_wave():
-  var enemies = num_enemies()
-
-  if enemies <= 0:
+func check_end_wave(delta):
+  if num_enemies <= 0:
     init_next_wave()
+  elif wave_time >= WAVE_TIMER:
+    init_next_wave()
+  else:
+    wave_time += delta
+
+func wave_interval_timer():
+  return WAVE_SPAWN_INTERVAL / (WAVE_SPAWNS_PER_INTERVAL * wave)
+
+func check_wave_spawns(delta):
+  if wave_interval_time >= wave_interval_timer():
+    wave_interval_time = 0
+    spawn_enemy(crawler_scene)
+  else:
+    wave_interval_time += delta
 
 func init_next_wave():
   is_next_wave = true
   wave += 1
-  wave_timer = 0
+  wave_time = 0
+  wave_wait_time = 0
+  wave_interval_time = 0
 
 func next_wave(delta):
-  if wave_timer <= WAVE_TIMER:
-    wave_timer += delta
+  if wave_wait_time <= WAVE_WAIT_TIMER:
+    wave_wait_time += delta
     return
 
   is_next_wave = false
 
-  for n in range(wave * WAVE_ENEMY_MULTIPLIER):
+  for _n in range(wave * WAVE_ENEMY_MULTIPLIER):
     spawn_enemy(crawler_scene)
 
 func _on_game_over_dialog_confirmed():
   restart()
 
 func restart():
-  get_tree().reload_current_scene()
+  var _scene = get_tree().reload_current_scene()
