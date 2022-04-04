aa55ca040875ce9d04848601792cf7cf8f05df5b
Ben Eng <whysguy52@gmail.com>
2022-04-04 15:46:24 -0500

    prototype death blood

--- objs/enemies/dead_alien.gd
+++ objs/enemies/dead_alien.gd
@@ -7,6 +7,10 @@ var time = 0
 func _ready():
   pass # Replace with function body.
 
+func _process(delta):
+  if time == 0:
+    $blood_particles.emitting = true
+
 func _physics_process(delta):
   if time >= MAX_TIME:
     remove_from_scene()
